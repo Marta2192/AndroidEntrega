197b487eae746d9cd0257471633f076cb64c3fcc
MartaGago <martagago92@hotmail.com>
2021-10-28 20:35:48 +0200

    TextView de contador en segunda escena

--- app/src/main/java/com/liceolapaz/dam/mgg/Login.java
+++ app/src/main/java/com/liceolapaz/dam/mgg/Login.java
@@ -14,67 +14,67 @@ import android.widget.TextView;
 
 
 
-    public class Login extends LinearLayout {
-        private EditText txtUsuario;
-        private EditText txtPassword;
-        private Button btnLogin;
-        private TextView lblMensaje;
+public class Login extends LinearLayout {
+    private EditText txtUsuario;
+    private EditText txtPassword;
+    private Button btnLogin;
+    private TextView lblMensaje;
 
-        private OnLoginListener listener;
+    private OnLoginListener listener;
 
-        public Login(Context context) {
-            super(context);
-            inicializar();
-        }
+    public Login(Context context) {
+        super(context);
+        inicializar();
+    }
 
-        public Login(Context context, AttributeSet attrs) {
-            super(context, attrs);
-            inicializar();
+    public Login(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        inicializar();
 
-            TypedArray a =
-                    getContext().obtainStyledAttributes(attrs,
-                            R.styleable.login_text);
+        TypedArray a =
+                getContext().obtainStyledAttributes(attrs,
+                        R.styleable.login_text);
 
-            String textoBoton = a.getString(
-                    R.styleable.login_text_login_text);
+        String textoBoton = a.getString(
+                R.styleable.login_text_login_text);
 
-            btnLogin.setText(textoBoton);
+        btnLogin.setText(textoBoton);
 
-            a.recycle();
-        }
+        a.recycle();
+    }
 
 
-        private void inicializar() {
+    private void inicializar() {
 
-            String infService = Context.LAYOUT_INFLATER_SERVICE;
-            LayoutInflater li =
-                    (LayoutInflater) getContext().getSystemService(infService);
-            li.inflate(R.layout.login, this, true);
+        String infService = Context.LAYOUT_INFLATER_SERVICE;
+        LayoutInflater li =
+                (LayoutInflater) getContext().getSystemService(infService);
+        li.inflate(R.layout.login, this, true);
 
-            txtUsuario = (EditText) findViewById(R.id.txtUsuario);
-            txtPassword = (EditText) findViewById(R.id.txtPassword);
-            btnLogin = (Button) findViewById(R.id.btnAceptar);
-            lblMensaje = (TextView) findViewById(R.id.lblMensaje);
+        txtUsuario = (EditText) findViewById(R.id.txtUsuario);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
+        btnLogin = (Button) findViewById(R.id.btnAceptar);
+        lblMensaje = (TextView) findViewById(R.id.lblMensaje);
 
-            asignarEventos();
-        }
+        asignarEventos();
+    }
 
-        public void setOnLoginListener(OnLoginListener l) {
-            listener = l;
-        }
+    public void setOnLoginListener(OnLoginListener l) {
+        listener = l;
+    }
 
-        private void asignarEventos() {
-            btnLogin.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    listener.onLogin(txtUsuario.getText().toString(),
-                            txtPassword.getText().toString());
-                }
-            });
-        }
+    private void asignarEventos() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                listener.onLogin(txtUsuario.getText().toString(),
+                        txtPassword.getText().toString());
+            }
+        });
+    }
 
-        public void setMensaje(String msg) {
-            lblMensaje.setText(msg);
-        }
+    public void setMensaje(String msg) {
+        lblMensaje.setText(msg);
+    }
 
 
 }
--- app/src/main/java/com/liceolapaz/dam/mgg/MainActivity.java
+++ app/src/main/java/com/liceolapaz/dam/mgg/MainActivity.java
@@ -26,34 +26,27 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-                    if (usuario.equals("admin") && password.equals("liceo"))
-                        ctlLogin.setMensaje("Login correcto!");
-                            Intent intent =
-                                    new Intent(MainActivity.this, Scene2.class);
+                if (usuario.equals("admin") && password.equals("liceo")) {
+                    ctlLogin.setMensaje("Login correcto!");
+                    Intent intent =
+                            new Intent(MainActivity.this, Scene2.class);
 
-                            //Creamos la información a pasar entre actividades
-                            Bundle b = new Bundle();
+                    Bundle b = new Bundle();
+
+                    intent.putExtras(b);
+
+                    startActivity(intent);
+
+                    finish();
+                }
+                if(!usuario.equals("admin") || !password.equals("liceo")) {
+                    ctlLogin.setMensaje("Usuario y/o contraseña incorrectos.");
+                    ++contador;
+                }
 
 
-                            //Añadimos la información al intent
-                            intent.putExtras(b);
-
-                            //Iniciamos la nueva actividad
-                            startActivity(intent);
-
-                            finish();
-
-                    if(!usuario.equals("admin") || !password.equals("liceo")) {
-                        ctlLogin.setMensaje("Usuario y/o contraseña incorrectos.");
-                        ++contador;
-                    }
-
-                
                 if((!usuario.equals("admin") || !password.equals("liceo")) && contador >=3)
                     System.exit(0);
-
-
-
 
 
 
@@ -63,25 +56,6 @@ public class MainActivity extends AppCompatActivity {
 
 
         });
-        /*btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Creamos el Intent
-                Intent intent =
-                        new Intent(MainActivity.this, AppBar.class);
-
-                //Creamos la información a pasar entre actividades
-                Bundle b = new Bundle();
-
-
-                //Añadimos la información al intent
-                intent.putExtras(b);
-
-                //Iniciamos la nueva actividad
-                startActivity(intent);
-            }
-        });*/
-
 
 
     }
